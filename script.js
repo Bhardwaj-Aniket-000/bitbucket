15e613fc94044d5355293df898d12dbe5469f3e4
Aniket-Bhardwaj <paniketbhardwaj676@gmail.com>
2024-05-08 18:52:13 +0530

    add foreach loop in script file

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 console.log("Aniket Bhardwaj");
-console.log("Git & GIthub tutorial")
+console.log("Git & GIthub tutorial");
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+arr.forEach((item) => {
+	console.log(item);
+});
